0d2c37059e3e1f6bd2240fbe0c4e81519e4a1a01
Kamil Bouhaouli <43292484+Kamilus2001@users.noreply.github.com>
2020-06-07 00:48:23 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 /* Here goes your JS code */
+//task 1 complete
+$("#show-popup-form").click(function(){
+	$(this).hide();
+	$("#login-form").show();
+
+});
+
+$("#exit-popup-form").click(function(){
+	$("#login-form").hide();
+	$("#show-popup-form").show();
+});
