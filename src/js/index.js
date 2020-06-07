900a19bcd767af9fbf37d87cd207c76bae019aa7
Kamil Bouhaouli <43292484+Kamilus2001@users.noreply.github.com>
2020-06-07 16:52:00 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 /* Here goes your JS code */
-//task 2 complete
+//task 3 complete
+var show=true;
 $("#show-popup-form").click(function(){
+	if(show){
 	$(this).hide();
 	$("#login-form").show();
-
+	}
 });
 
 $("#exit-popup-form").click(function(){
@@ -20,6 +22,14 @@ $("#submit").click(function(){
 			if(email=="kbouhaouli@gmail.com"&&pass=="12345678"){
 				$("#login-validation").css('color', 'green');
 				$("#login-validation").text("Success!");
+				setTimeout(function(){
+					$("#login-form").fadeOut(1000, function(){
+						show = false;
+						$("#show-popup-form").text("Thank You!");
+						$("#show-popup-form").fadeIn(1000);
+					});
+					
+					}, 2000);
 			}else{
 				$("#login-validation").text("Invalid email or password");
 			}
