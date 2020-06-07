9a3681312ba1df226ec2e9a745383c6af9264cd8
Kamil Bouhaouli <43292484+Kamilus2001@users.noreply.github.com>
2020-06-07 16:25:12 +0200

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 /* Here goes your JS code */
-//task 1 complete
+//task 2 complete
 $("#show-popup-form").click(function(){
 	$(this).hide();
 	$("#login-form").show();
@@ -9,4 +9,21 @@ $("#show-popup-form").click(function(){
 $("#exit-popup-form").click(function(){
 	$("#login-form").hide();
 	$("#show-popup-form").show();
+	$("#login-validation").text("");
+});
+$("#submit").click(function(){
+		var email = $("#email").val();
+		var pass = $("#pass").val();
+		var check = $("#accept");
+		$("#login-validation").css('color', 'red');
+		if(check.is(":checked")){
+			if(email=="kbouhaouli@gmail.com"&&pass=="12345678"){
+				$("#login-validation").css('color', 'green');
+				$("#login-validation").text("Success!");
+			}else{
+				$("#login-validation").text("Invalid email or password");
+			}
+		}else{
+			$("#login-validation").text("Check agree terms & conditions box");
+		}
 });
